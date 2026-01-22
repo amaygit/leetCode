cc1f56774645198e23244512461f99b67984e444
Amay <98589243+amaygit@users.noreply.github.com>
2026-01-22 09:56:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.22%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        double ans=1;
+        long b= n;
+        if(b<0){
+            x=1/x;
+            b=-b;
+        }
+        
+       while(b>0){
+        if(b%2==1)
+        ans*=x;
+        x*=x;
+        b/=2;
+       }
+       return ans;
     }
 };
