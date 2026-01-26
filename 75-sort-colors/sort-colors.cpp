3e77eea4795730d828c75d7349a7c2e2917fa5a3
Amay <98589243+amaygit@users.noreply.github.com>
2026-01-26 19:05:14 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.16%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        vector<int>arr;
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]==0)
+            arr.push_back(0);
+       }
+            for(int i=0;i<nums.size();i++){
+        if(nums[i]==1)            
+            arr.push_back(1);
+            }
+                 for(int i=0;i<nums.size();i++){
+            if(nums[i]==2)
+            arr.push_back(2);
+                 }
+
+       nums=arr;
+       
+     //  nums.clear();
+
       //  return nums;
     }
 };
