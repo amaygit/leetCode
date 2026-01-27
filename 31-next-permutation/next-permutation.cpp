1cb946f267c35cfe579ac24b3542fbf7102fe884
Amay <98589243+amaygit@users.noreply.github.com>
2026-01-27 20:08:57 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (91.96%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int indx = -1;
-
-        // 1️⃣ Find the pivot (first decreasing element from right)
-        for(int i = n - 2; i >= 0; i--) {
-            if(nums[i] < nums[i + 1]) {
-                indx = i;
-                break;
-            }
-        }
-
-        // 2️⃣ If no pivot found → array is in descending order
-        if(indx == -1) {
-            reverse(nums.begin(), nums.end());
+        int pivot=-1,index=-1;;
+      
+        int st=0,end=nums.size()-1;
+        if(nums.size()==2)
+        {
+            swap(nums[0],nums[1]);
             return;
         }
-
-        // 3️⃣ Find element just greater than pivot
-        for(int i = n - 1; i > indx; i--) {
-            if(nums[i] > nums[indx]) {
-                swap(nums[i], nums[indx]);
+        for(int i=nums.size()-1;i>0;i--){
+            if(nums[i-1]<nums[i]){
+                pivot=nums[i-1];
+               
+                index=i-1;
                 break;
             }
         }
+        if(index==-1){
+        reverse(nums.begin(),nums.end());
+        return ;
+        }
+        for(int i=nums.size()-1;i>0;i--){
+            if(pivot<nums[i]){
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+            reverse(nums.begin()+index+1,nums.end());
+        
 
-        // 4️⃣ Reverse the right side
-        reverse(nums.begin() + indx + 1, nums.end());
+        
+            
+
+
+        
     }
 };
