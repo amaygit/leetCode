43597a49b96aa0e57d521e1f28f1e614b254cb34
Amay <98589243+amaygit@users.noreply.github.com>
2026-01-28 17:19:49 +0530

    Time: 15 ms (5.40%) | Memory: 14.4 MB (5.02%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,13 +2,20 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         long long digit=0,ele=1,orginal=x;
+        vector<int>vec1;
+        vector<int>vec2;
+
         if(x<0)
         return false;
         while(x!=0){
             ele=x%10;
-            digit=digit*10+ele;
+            vec1.push_back(ele);
            x=x/10;
         }
-        return orginal==digit;
+        vec2=vec1;
+        reverse(vec1.begin(),vec1.end());
+        
+        
+        return vec1==vec2;
     }
 };
