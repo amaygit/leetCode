c5dcfde317664a75315318c4abafcb9ad21e0517
Amay <98589243+amaygit@users.noreply.github.com>
2026-01-29 17:17:28 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (54.30%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+bool isalnum(char ch){
+    if(ch>='0' && ch<='9' || ch>='A' && ch<='Z' || ch>='a' && ch<='z')
+    return true;
+    return false;
+}
     bool isPalindrome(string s) {
         int st=0,end=s.length()-1;
         while(st<end){
