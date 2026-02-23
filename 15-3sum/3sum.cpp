b5120b866fcf83482707c18cd8739b8ca4a69f78
Amay <98589243+amaygit@users.noreply.github.com>
2026-02-23 09:57:07 +0530

    Time: 62 ms (19.36%) | Memory: 31.1 MB (18.20%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -20,7 +20,7 @@ public:
     ans.push_back(pairs);
     j++,k--;
       while(j < k && nums[j] == nums[j-1]) j++;
-                    while(j < k && nums[k] == nums[k+1]) k--;
+                    //while(j < k && nums[k] == nums[k+1]) k--;
             }
         }
      }
