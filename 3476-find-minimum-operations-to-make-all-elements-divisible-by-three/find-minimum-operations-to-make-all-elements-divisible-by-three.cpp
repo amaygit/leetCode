07e641c24518ad105c094c2b0c5d1f970fa18dd6
Amay <98589243+amaygit@users.noreply.github.com>
2026-03-01 23:05:13 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (49.50%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
+int calc(vector<int>& nums,int n){
+    if(n==-1){
+        return 0;
+    }
+    if(nums[n]%3!=0)
+    return 1+calc(nums,n-1);
+    else
+      return calc(nums,n-1);
+}
     int minimumOperations(vector<int>& nums) {
-        int c=0;
-        for(int i=0;i<nums.size();i++){
-            if((nums[i]+1)%3==0 || (nums[i]-1)%3==0 )
-            c++;
-        }
-        return c;
+        return calc(nums,nums.size()-1);
+    
     }
 };
