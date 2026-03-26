90c9cce8b3b544ead88b828838800bc5ed2c88f2
Amay <98589243+amaygit@users.noreply.github.com>
2026-03-26 22:42:42 +0530

    Time: 13 ms (91.23%) | Memory: 59.7 MB (88.72%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long sum = 0;
+        for (auto b : batteries) sum += b;
+
+        long long low = 0, high = sum / n, ans = 0;
+
+        while (low <= high) {
+            long long mid = (low + high) / 2;
+
+            long long total = 0;
+            for (auto b : batteries) {
+                total += min((long long)b, mid);
+            }
+
+            if (total >= mid * n) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
