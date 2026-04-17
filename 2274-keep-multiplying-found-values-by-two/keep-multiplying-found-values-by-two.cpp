ce5abf4dd35c5727f4a05f34938fc386b0c4ceae
Amay <98589243+amaygit@users.noreply.github.com>
2026-04-17 23:26:16 +0530

    Time: 1 ms (48.48%) | Memory: 14 MB (75.21%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
-        sort(nums.begin(), nums.end());
-    for (int x : nums) {
-        if (x == original) original *= 2;
-    }
-    return original;
+        sort(nums.begin(),nums.end());
+        int num=INT_MIN;
+        bool flag=true;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==original || num==nums[i]){
+                num=nums[i]*2;
+                flag=false;
+            }
+        }
+        if(!flag)
+        return num;
+        return original;
     }
 };
