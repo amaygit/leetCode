5c40690927ae65ce8899607bcfdcb71ee16ee76b
Amay <98589243+amaygit@users.noreply.github.com>
2026-04-20 23:30:12 +0530

    Time: 99 ms (76.05%) | Memory: 172.8 MB (92.02%) - LeetSync

---
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int ans = 0;
+    int k;
+
+    long long dfs(int node, int parent, vector<vector<int>>& adj, vector<int>& values) {
+        long long sum = values[node];
+
+        for (auto &nei : adj[node]) {
+            if (nei == parent) continue;
+            sum += dfs(nei, node, adj, values);
+        }
+
+        if (sum % k == 0) {
+            ans++;
+            return 0; // cut here
+        }
+
+        return sum;
+    }
+
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges,
+                               vector<int>& values, int k) {
+        this->k = k;
+
+        vector<vector<int>> adj(n);
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        dfs(0, -1, adj, values);
+
+        return ans;
+    }
+};
