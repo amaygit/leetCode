f2b3ea053b3e4a2c160ab6c2ba8d1931dbea3717
Amay <98589243+amaygit@users.noreply.github.com>
2026-05-12 10:29:25 +0530

    Time: 0 ms (100.00%) | Memory: 31.9 MB (22.79%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-bool isValid(vector<vector<int>>&grid,int r,int c,int n,int expVal){
-    if(r<0 || c<0 || r>=n || c>=n || grid[r][c]!=expVal)
-    return false;
-    if(expVal==(n*n-1))return true;
-    int ans1=isValid(grid,r-2,c+1,n,expVal+1);
-    int ans2=isValid(grid,r-1,c+2,n,expVal+1);
-    int ans3=isValid(grid,r+1,c+2,n,expVal+1);
-    int ans4=isValid(grid,r+2,c+1,n,expVal+1);
-    int ans5=isValid(grid,r+2,c-1,n,expVal+1);
-    int ans6=isValid(grid,r+1,c-2,n,expVal+1);
-    int ans7=isValid(grid,r-1,c-2,n,expVal+1);
-    int ans8=isValid(grid,r-2,c-1,n,expVal+1);
-    return ans1||ans2||ans3||ans4||ans5||ans6||ans7||ans8;
+    bool check(vector<vector<int>>&grid,int row,int col,int n,int val){
+        if(row>=n || col>=n || row<0 || col<0 || grid[row][col]!=val ) return false;
 
-}
+        if(val==n*n-1)
+        return true;
+
+        int ans1=check(grid,row-2,col+1,n,val+1);
+        int ans2=check(grid,row-1,col+2,n,val+1);
+        int ans3=check(grid,row+1,col+2,n,val+1);
+        int ans4=check(grid,row+2,col+1,n,val+1);
+        int ans5=check(grid,row+2,col-1,n,val+1);
+        int ans6=check(grid,row+1,col-2,n,val+1);
+        int ans7=check(grid,row-1,col-2,n,val+1);
+        int ans8=check(grid,row-2,col-1,n,val+1);
+    return ans1||ans2||ans3||ans4||ans5||ans6||ans7||ans8;
+    }
     bool checkValidGrid(vector<vector<int>>& grid) {
-        return isValid(grid,0,0,grid.size(),0);
+        return check(grid,0,0,grid.size(),0);
     }
 };
