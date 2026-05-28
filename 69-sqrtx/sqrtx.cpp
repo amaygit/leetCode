34deb41ef76662425c57c26412baf76de43f3162
Amay <98589243+amaygit@users.noreply.github.com>
2026-05-28 10:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (49.02%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+       float m,n;
+       long long k=0;
+       if(x==1)
+       return 1;
+      
+       long long low=0,high=x;
+       while(low<=high){
+        long long mid=low+(high-low)/2;
+        if(mid*mid<=x)
+        {
+            k=mid;
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+       }
+        return k;
     }
 };
